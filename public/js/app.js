2cc5db64f741dbf0a365fa92c81b3d6b031a61b9
Kristjan Jansen <kristjan.jansen@gmail.com>
2013-08-28 14:33:15 +0300

    files...

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   page('/', data, renderFront, log)
-  page('/:dir', data, log)
+  page('/:dir', data, renderDir, log)
   page({dispatch: true})
 
 })
@@ -18,14 +18,24 @@ function data(ctx, next) {
 
 function log(ctx, next) {
   console.log(ctx.data)  
-  console.log(ctx.params.dir)  
-  console.log(ctx.params.file)  
   next()
 }
 
 function renderFront(ctx, next) {
   var output = Mustache.render($('#index').html(), ctx.data)
   $('body').html(output)
+  next()
+}
+
+function renderDir(ctx, next) {
+  $('body').append('hem')
+  
+  ctx.data.index.forEach(function(item) {
+    if (item.dir == ctx.params.dir) {
+      var output = Mustache.render($('#dir').html(), item)
+      $('body').html(output)
+    }
+  })
   next()
 }
 
