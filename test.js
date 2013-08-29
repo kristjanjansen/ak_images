79d20398d08643a84a40cfed1d0bb230818c9a36
Kristjan Jansen <kristjan.jansen@gmail.com>
2013-08-29 09:56:01 +0300

    moving

--- test.js
+++ test.js
@@ -6,16 +6,42 @@ var cron = require('cron').CronJob;
 var wrench = require('wrench')
 var im = require('imagemagick');
 
-var source = './files/source'
-var target = './files/target'
+var source = 'files/source'
+var target = 'files/target'
 var filetypes = /\.(jpg|jpeg|png|gif)$/i
 
 var cron = require('cron').CronJob;
-var job = new cron('*/10 * * * * *', processDir).start()
+//var job = new cron('*/10 * * * * *', processDir).start()
+
+var index = {}
+
+wrench.readdirSyncRecursive(source)
+.filter(function(f) {
+    return f.split('/').length == 2 && fs.statSync(path.join(source, f)).isFile() && f.match(filetypes)
+})
+.forEach(function (f) {
+  var dir = f.split('/')[0]
+  var file = f.split('/')[1]
+
+  if (!index[dir]) {
+    index[dir] = {}
+    index[dir].files = []
+  } 
+  
+  index[dir].files.push(file)
+
+})
+console.log(index)
+
+
+
+
 
 function processDir() {
   
 var queue = []
+
+// Travel the source directories and find images not yet converted
 
 wrench.readdirSyncRecursive('./files/source')
 .filter(function(f) {
@@ -24,6 +50,8 @@ wrench.readdirSyncRecursive('./files/source')
 .forEach(function (f) {
   if (!fs.existsSync(path.join(target, f))) queue.push(f)
 })
+
+// Process each image in queue
 
 each(queue)
 .on('item', function(f, i, next) {
